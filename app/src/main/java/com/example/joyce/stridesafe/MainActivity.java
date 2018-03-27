1328106b01aea4c5737164310d3761e0febf268a
rwmehta <ramehta@wpi.edu>
2018-03-27 12:43:22 -0400

    Wassup dude

--- app/src/main/java/com/example/joyce/stridesafe/MainActivity.java
+++ app/src/main/java/com/example/joyce/stridesafe/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Hi, this is my comment.........
+        // sah dude
     }
 }
