2c72bb806da00b27850d03c58503f265b5636079
rwmehta <ramehta@wpi.edu>
2018-03-28 08:51:30 -0400

    IIS

--- app/src/main/java/com/example/joyce/stridesafe/MainActivity.java
+++ app/src/main/java/com/example/joyce/stridesafe/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Hi, this is my comment.........
         // sah dude
+        // IIW IIS
     }
 }
