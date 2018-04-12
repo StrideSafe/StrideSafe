15f4a90fcb6a2ec13be5cc4a066bf0ae8bea6f0d
itihaas <itihaas.k@gmail.com>
2018-04-12 15:42:33 -0400

    Added PolyLine in New York

--- app/src/main/java/com/example/joyce/stridesafe/MapTest.java
+++ app/src/main/java/com/example/joyce/stridesafe/MapTest.java
@@ -1,55 +1,41 @@
 package com.example.joyce.stridesafe;
-
-
-
 import android.Manifest;
-
 import android.content.pm.PackageManager;
-
+import android.graphics.Color;
 import android.location.Location;
-
 import android.support.annotation.NonNull;
-
 import android.support.v4.app.ActivityCompat;
-
 import android.support.v4.app.FragmentActivity;
-
 import android.os.Bundle;
-
 import android.util.Log;
-
 import android.widget.Toast;
-
-
-
-//import com.google.android.gms.maps.CameraUpdateFactory;
-
 import com.google.android.gms.maps.GoogleMap;
-
 import com.google.android.gms.maps.OnMapReadyCallback;
-
 import com.google.android.gms.maps.SupportMapFragment;
-
 import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
-
 import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.CameraUpdateFactory;
 
-//import com.google.android.gms.maps.model.LatLng;
-
-//import com.google.android.gms.maps.model.MarkerOptions;
-
+import java.util.ArrayList;
 
 
 public class MapTest extends FragmentActivity implements OnMyLocationButtonClickListener,
 
+
+
         OnMyLocationClickListener, OnMapReadyCallback {
 
+    private static final LatLng LOWER_MANHATTAN = new LatLng(40.722543,
+            -73.998585);
+    private static final LatLng TIMES_SQUARE = new LatLng(40.7577, -73.9857);
+    private static final LatLng BROOKLYN_BRIDGE = new LatLng(40.7057, -73.9964);
 
 
     private GoogleMap mMap;
-
-
-
+    ArrayList<LatLng> markerPoints;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,43 +43,20 @@ public class MapTest extends FragmentActivity implements OnMyLocationButtonClick
         Log.d("Map Entry", "I have entered the onCreate Method");
 
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_map_test);
 
+        // Initializing array List
+        markerPoints = new ArrayList<LatLng>();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
         mapFragment.getMapAsync(this);
-
+//        setUpMapIfNeeded();
     }
 
 
-
-
-
-    /**
-
-     * Manipulates the map once available.
-
-     * This callback is triggered when the map is ready to be used.
-
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-
-     * we just add a marker near Sydney, Australia.
-
-     * If Google Play services is not installed on the device, the user will be prompted to install
-
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-
-     * installed Google Play services and returned to the app.
-
-     */
-
-    @Override
-
+        @Override
     public void onMapReady(GoogleMap googleMap) {
 
         mMap = googleMap;
@@ -123,7 +86,7 @@ public class MapTest extends FragmentActivity implements OnMyLocationButtonClick
         mMap.setOnMyLocationButtonClickListener(this);
 
         mMap.setOnMyLocationClickListener(this);
-
+        setUpMap();
     }
 
     public boolean checkLocationPermission() {
@@ -190,6 +153,28 @@ public class MapTest extends FragmentActivity implements OnMyLocationButtonClick
 
     }
 
+    private void setUpMap() {
+        // check if we have got the googleMap already
+      //  if (mMap == null) {
+      //      mMap = ((SupportMapFragment) getSupportFragmentManager()
+      //              .findFragmentById(R.id.map)).getMapAsync(this);
+            if (mMap != null) {
+                addLines();
+            }
 
+            //      mMap.setOnCameraChangeListener(this);
+        }
+
+    private void addLines() {
+
+        mMap
+                .addPolyline((new PolylineOptions())
+                        .add(TIMES_SQUARE, BROOKLYN_BRIDGE, LOWER_MANHATTAN,
+                                TIMES_SQUARE).width(5).color(Color.BLUE)
+                        .geodesic(true));
+        // move camera to zoom on map
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LOWER_MANHATTAN,
+                13));
+    }
 
 }
