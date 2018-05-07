f68ee0532fc2022ed2c91c2f639407545cc4f92c
itihaas <itihaas.k@gmail.com>
2018-05-07 09:30:23 -0400

    Lat Long Stuff

--- app/src/main/java/com/example/joyce/stridesafe/DecisionsActivity.java
+++ app/src/main/java/com/example/joyce/stridesafe/DecisionsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class DecisionsActivity extends AppCompatActivity {
 
@@ -16,5 +17,29 @@ public class DecisionsActivity extends AppCompatActivity {
     public void Map(View view) {
         Intent intent = new Intent(this, MapTest.class);
         startActivity(intent);
+    }
+
+    public String getStartLoc(View view)
+    {
+        EditText userStart = findViewById(R.id.UserStartLoc);
+        String address1 = userStart.getText().toString();
+        return address1;
+    }
+
+    public String getEndLoc(View view)
+    {
+        EditText userEnd = findViewById(R.id.UserEndLoc);
+        String address2 = userEnd.getText().toString();
+        return address2;
+    }
+
+    public void sendAddresses(View view)
+    {
+        String address1 = getStartLoc(view);
+        String address2 = getEndLoc(view);
+
+        Intent intent = new Intent(this, MapTest.class);
+        intent.putExtra("UserStartLoc", address1);
+        intent.putExtra("UserEndLoc", address2);
     }
 }
--- app/src/main/java/com/example/joyce/stridesafe/MapTest.java
+++ app/src/main/java/com/example/joyce/stridesafe/MapTest.java
@@ -1,185 +1,197 @@
 package com.example.joyce.stridesafe;
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.icu.text.DecimalFormat;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.os.Bundle;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
-import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.CameraUpdateFactory;
-
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-
-
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
 public class MapTest extends FragmentActivity implements OnMyLocationButtonClickListener,
-
-
-
-        OnMyLocationClickListener, OnMapReadyCallback {
-
+        OnMapReadyCallback {
     private static final LatLng Prescott_A = new LatLng(42.27385542588484, -71.79915435938568);
     private static final LatLng Prescott_B = new LatLng(42.27907885741453, -71.7996264281723);
-
-
     private GoogleMap mMap;
     ArrayList<LatLng> markerPoints;
+    int mapLocation = 0;
+    private LocationRequest mLocationRequest;
+    private long UPDATE_INTERVAL = 10 * 1000; /* 10 secs */
+    private long FASTEST_INTERVAL = 2000; /* 2 sec */
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
-
         Log.d("Map Entry", "I have entered the onCreate Method");
-
         super.onCreate(savedInstanceState);setContentView(R.layout.activity_map_test);
-
-        // Initializing array List
+// Initializing array List
         markerPoints = new ArrayList<LatLng>();
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+// Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-
         mapFragment.getMapAsync(this);
-//        setUpMapIfNeeded();
+// setUpMapIfNeeded();
     }
-
-
-        @Override
+    @Override
     public void onMapReady(GoogleMap googleMap) {
-
         mMap = googleMap;
-
         Log.d("Map Entry", "I have entered the onMapReady Method");
-
-        /* Sample Code, We may want to reference later.
-
-        LatLng sydney = new LatLng(-34, 151);
-
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
-
-
-
+/* Sample Code, We may want to reference later.
+LatLng sydney = new LatLng(-34, 151);
+mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
         boolean check = checkLocationPermission();
-
         if(!check){
-
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-
         }
-
         mMap.setMyLocationEnabled(true);
-
         mMap.setOnMyLocationButtonClickListener(this);
-
-        mMap.setOnMyLocationClickListener(this);
         setUpMap();
+        getLastLocation();
+        startLocationUpdates();
     }
-
     public boolean checkLocationPermission() {
-
         String permission = "android.permission.ACCESS_FINE_LOCATION";
-
         int res = this.checkCallingOrSelfPermission(permission);
-
         return (res == PackageManager.PERMISSION_GRANTED);
-
     }
-
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-
         switch (requestCode) {
-
             case 1: {
-
-                // If request is cancelled, the result arrays are empty.
-
-                if (grantResults.length > 0
-
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-
-
-                } else {
-
-                    // permission denied, boo! Disable the
-
-                    // functionality that depends on this permission.
-
+// If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 }
-
+                else {
+// permission denied, boo! Disable the
+// functionality that depends on this permission.
+                }
                 return;
-
             }
-
         }
-
     }
-
-    @Override
-
     public void onMyLocationClick(@NonNull Location location) {
-
         Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
-
     }
-
-
-
     @Override
-
     public boolean onMyLocationButtonClick() {
-
         Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
-
-        // Return false so that we don't consume the event and the default behavior still occurs
-
-        // (the camera animates to the user's current position).
-
+// Return false so that we don't consume the event and the default behavior still occurs
+// (the camera animates to the user's current position).
         return false;
-
     }
-
-    private void setUpMap() {
-        // check if we have got the googleMap already
-      //  if (mMap == null) {
-      //      mMap = ((SupportMapFragment) getSupportFragmentManager()
-      //              .findFragmentById(R.id.map)).getMapAsync(this);
-            if (mMap != null) {
-                addLines();
-            }
-
-            //      mMap.setOnCameraChangeListener(this);
+    // Trigger new location updates at interval
+    protected void startLocationUpdates() {
+        // Create the location request to start receiving updates
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+        // Create LocationSettingsRequest object using location request
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+        // Check whether location settings are satisfied
+        // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+        settingsClient.checkLocationSettings(locationSettingsRequest);
+        boolean check = checkLocationPermission();
+        if (!check) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
-
-    private void addLines() {
-
-        mMap
-                .addPolyline((new PolylineOptions())
-                        .add(Prescott_A,Prescott_B).width(5).color(Color.BLUE)
-                        .geodesic(true));
-        // move camera to zoom on map
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Prescott_A, 13));
+        // new Google API SDK v11 uses getFusedLocationProviderClient(this)
+        getFusedLocationProviderClient(this).requestLocationUpdates(mLocationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                    // do work here
+                        onLocationChanged(locationResult.getLastLocation());
+                    }
+                },
+                Looper.myLooper());
+    }
+    public void onLocationChanged(Location location) {
+        // New location has now been determined
+        String msg = "Updated Location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
+    public void getLastLocation() {
+        // Get last known recent location using new Google Play Services SDK (v11+)
+        FusedLocationProviderClient locationClient = getFusedLocationProviderClient(this);
+        boolean check = checkLocationPermission();
+        if (!check) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+        locationClient.getLastLocation()
+                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // GPS location can be null if GPS is switched off
+                        if (location != null) {
+                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            onLocationChanged(location);
+                            Log.d("mapActivity", "New Location Coordinates: " + latLng);
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("mapActivity", "Error trying to get last GPS location");
+                        e.printStackTrace();
+                    }
+                });
+    }
+    private void setUpMap() {
+        if(mMap !=null) {
+            addLines();
+        }
+    }
+    private void addLines() {
+        mMap.addPolyline((new PolylineOptions()).add(Prescott_A, Prescott_B).width(5).color(Color.BLUE).geodesic(true));
+        // move camera to zoom on map
+        float zoomLevel = 16.0f; //This goes up to 21
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Prescott_A, zoomLevel));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Prescott_A, 13));
+    }
+
+    public List getLatLngs ()
+    {
+        Bundle bundle = getIntent().getExtras();
+
+        String Loc1 = bundle.getString("UserStartLoc");
+        String Loc2 = bundle.getString("UserEndLoc");
+        LatLng start = getLocationFromAddress(Loc1);
+        LatLng end = getLocationFromAddress(Loc2);
+        List<LatLng> list = new ArrayList<LatLng>();
+        list.add(start);
+        list.add(end);
+        return list;
+    }
 
 
     public void CalculationByDistance(View view) {
@@ -197,35 +209,26 @@ public class MapTest extends FragmentActivity implements OnMyLocationButtonClick
         double c = 2 * Math.asin(Math.sqrt(a));
         Double valueResult = Radius * c;
         valueResult = valueResult * 3280.84; // KM To FEET
-//        Log.i("Radius Value", "" + valueResult + "   KM  " + kmInDec
-//                + " Meter   " + meterInDec);
-        
+        //Log.i("Radius Value", "" + valueResult + " KM " + kmInDec + " Meter " + meterInDec);
         String dist = valueResult.toString();
         Toast.makeText(this, dist, Toast.LENGTH_SHORT).show();
-
     }
 
     public LatLng getLocationFromAddress(String strAddress) {
-
         Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-
         try {
             // May throw an IOException
             List<Address> addresses = geocoder.getFromLocationName(strAddress, 1);
             Address address = addresses.get(0);
-
             if (addresses.size() > 0) {
                 double latitude = addresses.get(0).getLatitude();
                 double longitude = addresses.get(0).getLongitude();
                 LatLng p1 = new LatLng(latitude, longitude);
                 return p1;
             }
-
         } catch (IOException ex) {
-
             ex.printStackTrace();
         }
         return null;
     }
-
 }
